0264e7c6ef3a210853f376558032fc3e1cd1ed2f
Motocarota <motocarota@gmail.com>
2024-03-07 18:59:35 +0100

    boom

--- src/lib/Portfolio.svelte
+++ src/lib/Portfolio.svelte
@@ -1,35 +1,64 @@
 <script>
-  
+  import _chunk from 'lodash/chunk';
+  import { Modal } from 'flowbite-svelte';  
 	import Slider from "../lib/Slider.svelte";
 
+  let currentSection = null;
+
   const sections = [
-    { id: 'cancelli-di-sicurezza', img: 10 },
     { id: 'oscuranti-zanzariere-velux', img: 6 },
     { id: 'tapparelle', img: 10 },
     { id: 'tende-da-sole', img: 10 },
     { id: 'zanzariere-finestre', img: 5 },
     { id: 'zanzariere-plissettate', img: 9 },
     { id: 'zanzariere-portefinestre', img: 10 },
+    { id: 'cancelli-di-sicurezza', img: 10 },
   ]
 
-  function getImages(section) {
-    return new Array(section.img).fill(0).map(
+  function getImages({ id, img }) {
+    return new Array(img).fill(0).map(
       (_, index) => {
-        const label = section.id.split('-').join(' ')
-
         return {
-          alt: label,
-          src: `/img/${section.id}/${index + 1}.webp`,
-          title: label
+          alt: id,
+          src: `/img/${id}/${index + 1}.webp`,
+          title: id
         }
       }
     )
   }
 
+  function getGalleryImages(section) {
+    if (!section) {
+      currentSection = null
+      return
+    }
+    
+    currentSection = {
+      ...section,
+      label: section.title,
+      images: section.images,
+    }
+  }
+
+  const images = sections.map(
+    ({ id, img }) => ({
+      alt: id,
+      src: `/img/${id}/1.webp`,
+      title: id.replaceAll('-', ' '),
+      images: getImages({ id, img })
+    })
+  )
 </script>
 
-<div id="services" class="max-w-screen-lg mx-auto">
-  {#each sections as section}
-    <Slider title={section.id.split('-').join(' ')} images={getImages(section)}/>
+<div id="services" class="max-w-screen-lg mx-auto my-5 flex flex-wrap justify-center">
+  {#each images as i}
+    <button on:click={() => getGalleryImages(i)} class="m-5 rounded h-56 overflow-hidden">
+      {i.title}
+      <img src={i.src} alt={i.id} width={300} />
+    </button>
   {/each}
 </div>
+
+<Modal bind:open={currentSection} outsideclose size="xl" class="bg-slate-800">
+  <Slider title={currentSection.title} images={currentSection.images} />
+</Modal>
--- src/lib/Slider.svelte
+++ src/lib/Slider.svelte
@@ -1,13 +1,23 @@
 <script>
-  import { Carousel } from 'flowbite-svelte';
-
   export let title;
   export let images;
+
+  let index = 0;
+
+  function next() {
+    console.log('n', {index})
+
+    index = (index < images.length -1) ? index +1 : 0
+  }
+  function prev() {
+    console.log('p', {index})
+    index = index > 0 ? (index-1) : index
+  }
+
 </script>
 
-<div class="max-w-4xl mx-auto">
-  <h2 class="capitalize mt-5">{title}</h2>
-  <Carousel {images} let:Controls let:Indicators>
-    <Controls />
-  </Carousel>
+<div class="flex justify-center">
+  <button on:click={prev}> &lt; </button>
+  <img class="mx-auto h-max" src={images[index].src} alt={images[index].alt}>
+  <button on:click={next}> &gt; </button>
 </div>
