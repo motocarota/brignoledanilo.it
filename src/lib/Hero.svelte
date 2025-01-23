e9a54787baf7c2531ef3149a9ad21525e0e448af
Motocarota <motocarota@gmail.com>
2025-01-23 18:31:20 +0100

    foto

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,8 +1,14 @@
-<section id="top" style="background-image: url(img/bg.webp); background-repeat: no-repeat; background-position: center; background-size: cover;">
-  <div class="px-10 py-20 text-white max-w-screen-lg mx-auto" style="filter: drop-shadow(2px 4px 6px black);">
-    <h1 class="font-size-56">
-      Tende da sole <br>Pergotende<br> Serrande<br> Tapparelle<br> Zanzariere<br>
-    </h1>
-    <h2 class="mt-5">Preventivi gratuiti, riparazioni, motorizzazioni</h2>
-  </div>
+<section
+	id="top"
+	style="background-image: url(img/hero.webp); background-repeat: no-repeat; background-position: center; background-size: cover;"
+>
+	<div
+		class="px-10 py-20 text-white max-w-screen-lg mx-auto"
+		style="filter: drop-shadow(2px 4px 6px black);"
+	>
+		<h1 class="font-size-56">
+			Tende da sole <br />Pergotende<br /> Serrande<br /> Tapparelle<br /> Zanzariere<br />
+		</h1>
+		<h2 class="mt-5">Preventivi gratuiti, riparazioni, motorizzazioni</h2>
+	</div>
 </section>
--- src/lib/Portfolio.svelte
+++ src/lib/Portfolio.svelte
@@ -1,64 +1,58 @@
 <script>
-  import _chunk from 'lodash/chunk';
-  import Slider from './Slider.svelte';
+	import _chunk from 'lodash/chunk';
+	import Slider from './Slider.svelte';
 
-  let currentSection = null;
+	let currentSection = null;
 
-  const sections = [
-    { id: 'oscuranti-zanzariere-velux', img: 6 },
-    { id: 'pergotende', img: 2 },
-    { id: 'tapparelle', img: 10 },
-    { id: 'tende-da-sole', img: 10 },
-    { id: 'tende-laterali-crystal', img: 5 },
-    { id: 'zanzariere-finestre', img: 4 },
-    { id: 'zanzariere-plissettate', img: 16 },
-    { id: 'zanzariere-portefinestre', img: 10 },
-    { id: 'cancelli-di-sicurezza', img: 10 },
-  ]
+	const sections = [
+		{ id: 'tende-da-sole', img: 9 },
+		{ id: 'tapparelle', img: 10 },
+		{ id: 'pergotende', img: 2 },
+		{ id: 'tende-laterali-crystal', img: 5 },
+		{ id: 'zanzariere-finestre', img: 2 },
+		{ id: 'zanzariere-portefinestre', img: 10 },
+		{ id: 'zanzariere-plissettate', img: 16 },
+		{ id: 'cancelli-di-sicurezza', img: 10 },
+		{ id: 'oscuranti-zanzariere-velux', img: 6 }
+	];
 
-  function getImages({ id, img }) {
-    return new Array(img).fill(0).map(
-      (_, index) => {
-        return {
-          alt: id,
-          src: `/img/${id}/${index + 1}.webp`,
-          title: id
-        }
-      }
-    )
-  }
+	function getImages({ id, img }) {
+		return new Array(img).fill(0).map((_, index) => {
+			return {
+				alt: id,
+				src: `/img/${id}/${index + 1}.webp`,
+				title: id
+			};
+		});
+	}
 
-  function getGalleryImages(section) {
-    if (!section) {
-      currentSection = null
-      return
-    }
-    
-    return {
-      label: section.title,
-      images: section.images,
-    }
-  }
+	function getGalleryImages(section) {
+		if (!section) {
+			currentSection = null;
+			return;
+		}
 
-  const images = sections.map(
-    ({ id, img }) => ({
-      alt: id,
-      src: `/img/${id}/1.webp`,
-      title: id.replaceAll('-', ' '),
-      images: getImages({ id, img })
-    })
-  )
+		return {
+			label: section.title,
+			images: section.images
+		};
+	}
 
-  const galleryImages = images.map(
-    (i) => getGalleryImages(i)
-  )
-  console.log({images, galleryImages})
+	const images = sections.map(({ id, img }) => ({
+		alt: id,
+		src: `/img/${id}/1.webp`,
+		title: id.replaceAll('-', ' '),
+		images: getImages({ id, img })
+	}));
+
+	const galleryImages = images.map((i) => getGalleryImages(i));
+	console.log({ images, galleryImages });
 </script>
 
 <h2 class="text-center my-4">Guarda la gallery</h2>
 <h5 class="text-center">Clicca per visualizzare altre foto</h5>
 <div id="services" class="max-w-screen-lg mx-auto my-5 flex flex-wrap justify-center align-middle">
-  {#each galleryImages as i}
-    <Slider images={i.images} label={i.label}/>
-  {/each}
+	{#each galleryImages as i}
+		<Slider images={i.images} label={i.label} />
+	{/each}
 </div>
