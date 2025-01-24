a745644cad9ee5e413293de1c62e966e79fca6ef
Motocarota <motocarota@gmail.com>
2025-01-24 10:15:03 +0100

    lint

--- src/lib/Slider.svelte
+++ src/lib/Slider.svelte
@@ -1,30 +1,30 @@
 <script>
-  import LeftIcon from 'flowbite-svelte-icons/AngleLeftOutline.svelte'
-  import RightIcon from 'flowbite-svelte-icons/AngleRightOutline.svelte'
+	import LeftIcon from 'flowbite-svelte-icons/AngleLeftOutline.svelte';
+	import RightIcon from 'flowbite-svelte-icons/AngleRightOutline.svelte';
 
-  export let images;
-  export let label;
+	export let images;
+	export let label;
 
-  let index = 0;
+	let index = 0;
 
-  function next() {
-    console.log('N',{index})
-    index = (index < images.length - 1) ? index + 1 : 0
-  }
+	function next() {
+		// console.log('Next', { index });
+		index = index < images.length - 1 ? index + 1 : 0;
+	}
 
-  function prev() {
-    console.log('P',{index})
-    index = index > 0 ? (index - 1) : index
-  }
+	function prev() {
+		// console.log('Prev', { index });
+		index = index > 0 ? index - 1 : index;
+	}
 </script>
 
 <div class="my-5">
-  <div class="flex justify-center align-middle" style="max-height: 226px; overflow: hidden;">
-    <button on:click={prev}> <LeftIcon /> </button>
-    <a href={images[index]?.src ?? "#"} target="_blank"> 
-      <img src={images[index]?.src ?? ""} alt={images[index]?.alt ?? ""} width="300">
-    </a>
-    <button on:click={next}> <RightIcon /> </button>
-  </div>
-  <p class="text-center text-slate-600">{label}</p>
+	<div class="flex justify-center align-middle" style="max-height: 226px; overflow: hidden;">
+		<button on:click={prev}> <LeftIcon /> </button>
+		<a href={images[index]?.src ?? '#'} target="_blank">
+			<img src={images[index]?.src ?? ''} alt={images[index]?.alt ?? ''} width="300" />
+		</a>
+		<button on:click={next}> <RightIcon /> </button>
+	</div>
+	<p class="text-center text-slate-600">{label}</p>
 </div>
